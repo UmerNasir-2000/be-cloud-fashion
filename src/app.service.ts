d5fb876f7a7d6732c7ab4421458a3a887834faac
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2023-08-25 19:31:01 +0500

    [FEAT] Added route for fetching products 🚧

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from '@prisma/client';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'The Visionary!!!!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  getProducts(): Promise<Product[]> {
+    return this.prisma.product.findMany({ take: 10 });
   }
 }
